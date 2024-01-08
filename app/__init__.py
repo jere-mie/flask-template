a5efc076da559a1cd42df824bb94ab1478b6a903
Jeremie Bornais <jeremiejbornais@gmail.com>
2024-01-08 19:45:10 +0000

    small adjustments

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,6 @@ def create_app(config_object=Config):
 
     @app.route('/test/')
     def test_page():
-        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+        return '<h1>It Works!</h1>'
 
     return app
